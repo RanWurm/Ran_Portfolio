690e873ec271e7ebca2133a3c8107d677451b884
RanWurm <ranwurembrand@gmail.com>
2024-05-05 17:25:09 +0300

    minor changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,8 +20,8 @@ const Homepage = () => {
     <h1 className="text-4xl md:text-5xl font-bold z-40">
      Crafting Digital Experiences, Designing Tomorrow.
     </h1>
-    {/* DESC */}
-    <p className="md:text-xl z-40">
+    {/* DESC md:text-xl z-40"*/}
+    <p className="hidden sm:block sm:text-xl z-40">
       Welcome to my digital canvas, where innovation and creativity
       converge. With a keen eye for aesthetics and a mastery of code, my
       portfolio showcases a diverse collection of projects that reflect my
